008c5905888979dc36d8ec5c4d31c7c0f97b14d8
kajal <tiwarikajal1306@gmail.com>
2020-05-01 19:51:51 +0530

    added UC6

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+declare -A result
 read -p "Take First input: " a
 read -p "Take Second input: " b
 read -p "Take Third input: " c
@@ -6,3 +7,15 @@ d=$(($a+$b*$c))
 e=$(($a*$b+$c))
 f=$(($c+$a/$b))
 g=$(($a%$b+$c))
+result[a+b*c]="$d"
+result[a*b+c]="$e"
+result[c+a/b]="$f"
+result[a%b+c]="$g"
+echo "Result of first computation is: "  ${result[a+b*c]}
+echo "Result of second computation is: " ${result[a*b+c]}
+echo "Result of third computation is: " ${result[c+a/b]}
+echo "Result of forth computation is: " ${result[a%b+c]}
+
+
+
+
